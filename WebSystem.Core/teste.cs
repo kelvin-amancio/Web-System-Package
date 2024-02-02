71bb028d66106c6e59b1bafcd486ba0a8db00f26
Kelvin de Almeida Amancio <89745630+kelvin-amancio@users.noreply.github.com>
2024-02-02 16:36:10 -0300

    Delete WebSystem.Core/teste.cs

--- WebSystem.Core/teste.cs
+++
@@ -1,9 +0,0 @@
-
-namespace WebSystem.Core
-{
-    public class Teste
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Desc { get; set; } = string.Empty;
-    }
-}
