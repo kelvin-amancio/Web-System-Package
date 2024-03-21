f62f027c08f1f0e42b7484385ec589486ad2c7c2
kelvin-amancio <kelvinamancio10@gmail.com>
2024-03-21 10:44:59 -0300

    Update WebSystem Token

---
+++ WebSystem.Core/WebSystemObject.cs
@@ -0,0 +1,13 @@
+
+namespace WebSystem.Core
+{
+    public class WebSystemObject
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public string[]? Roles { get; set; }
+    }
+}
